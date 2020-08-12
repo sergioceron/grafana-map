3b742146928671b436e467df738329083dd1e209
Sergio Ceron <root@xell.dev>
2020-08-12 14:42:08 -0500

    Fixed Refreshing Map

--- src/MapPanel.tsx
+++ src/MapPanel.tsx
@@ -1,24 +1,139 @@
 import React, { PureComponent } from 'react';
 import { PanelProps } from '@grafana/data';
-import { MapOptions } from './types';
+import { MapOptions, MapStyle } from './types';
 import { css } from 'emotion';
 
-// eslint-disable-next-line
-import { WebMapView } from './WebMapView';
+import ethers from 'ethers';
+// @ts-ignore
+import request from 'request';
+// @ts-ignore
+import nodeIngressABI from './contracts/NodeIngress.json';
+// @ts-ignore
+import nodeListABI from './contracts/NodeList.json';
+import { loadCss, loadModules } from 'esri-loader';
+import MapLayer from './MapLayer';
 
 interface Props extends PanelProps<MapOptions> {}
 interface State {
   // eslint-disable-next-line
 }
 
+const ipv4Prefix = '00000000000000000000ffff';
+
+const getIPLocation = (ip: string) =>
+  new Promise(resolve => {
+    let url = `http://api.ipstack.com/${ip}?access_key=67332e46b2ec77d406cffe607d152297`;
+    request(
+      {
+        url: url,
+        method: 'GET',
+        json: true,
+      },
+      (error: any, response: any, body: any) => {
+        if (response.statusCode !== 200) {
+          resolve('FAILED');
+        }
+
+        if (body.success === false) {
+          resolve('LIMIT_REACHED');
+        }
+
+        if (typeof body.latitude !== 'undefined') {
+          resolve({ latitude: body.latitude, longitude: body.longitude });
+        }
+        resolve('FAILED');
+      }
+    );
+  });
+
+const splitAddress = (address: string, digits: number) => {
+  const bits: string[] = [];
+
+  while (address.length >= digits) {
+    bits.push(address.slice(0, digits));
+    address = address.slice(digits);
+  }
+  return bits;
+};
+
+const getIpv4 = (address: string) => {
+  return splitAddress(address.split(ipv4Prefix)[1], 2)
+    .map(hex => {
+      return parseInt(hex, 16);
+    })
+    .join('.');
+};
+
 export class MapPanel extends PureComponent<Props, State> {
-  render() {
-    const { options, width, height } = this.props;
+  mapRef = React.createRef<HTMLDivElement>();
+
+  componentDidMount() {
+    console.log('Panel Mounted');
+    const { options } = this.props;
     const {
       style,
       besuSettings: { ingressContractAddress, rpcUrl },
       mapSettings: { nodeMarkerColor },
     } = options;
+    const nodeIngress = new ethers.Contract(
+      ingressContractAddress,
+      nodeIngressABI,
+      new ethers.providers.JsonRpcProvider(rpcUrl)
+    );
+
+    loadCss(`https://js.arcgis.com/4.16/esri/themes/${style}/main.css`, 'style');
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/geometry/Point'], {
+      css: false,
+    }).then(([ArcGISMap, MapView, FeatureLayer, Point]) => {
+      const map = new ArcGISMap({
+        basemap: style === MapStyle.dark ? 'dark-gray' : 'gray',
+      });
+
+      const view = new MapView({
+        container: this.mapRef.current,
+        map: map,
+        center: [0, 34],
+        zoom: 2,
+      });
+      const mapLayer = new MapLayer(nodeMarkerColor);
+      const layer = new FeatureLayer(mapLayer.getLayer());
+      map.add(layer);
+
+      nodeIngress
+        .getContractAddress('0x72756c6573000000000000000000000000000000000000000000000000000000')
+        .then(async (address: string) => {
+          const nodeList = new ethers.Contract(address, nodeListABI, new ethers.providers.JsonRpcProvider(rpcUrl));
+          const size = parseInt(await nodeList.getSize(), 10);
+          for (let i = 1; i < size; i++) {
+            nodeList.getByIndex(i).then(async (whitelist: any) => {
+              const ipHex = whitelist[2];
+              const ip = getIpv4(ipHex.split('x')[1]);
+              // @ts-ignore
+              const { longitude, latitude } = await getIPLocation(ip);
+              await layer.applyEdits({
+                addFeatures: [
+                  {
+                    geometry: new Point({ y: latitude, x: longitude }),
+                    attributes: { ObjectID: i, ip, lon: longitude, lat: latitude },
+                  },
+                ],
+              });
+            });
+          }
+          // @ts-ignore
+        });
+
+      return () => {
+        if (view) {
+          // destroy the map view
+          view.container = null;
+        }
+      };
+    });
+  }
+
+  render() {
+    const { width, height } = this.props;
 
     const clazz = css`
       display: flex;
@@ -36,12 +151,7 @@ export class MapPanel extends PureComponent<Props, State> {
           height,
         }}
       >
-        <WebMapView
-          style={style}
-          ingressContractAddress={ingressContractAddress}
-          rpcUrl={rpcUrl}
-          nodeMarkerColor={nodeMarkerColor}
-        />
+        <div className="webmap" style={{ width: '100%', height: '300px', backgroundColor: '#FFF' }} ref={this.mapRef} />
       </div>
     );
   }
